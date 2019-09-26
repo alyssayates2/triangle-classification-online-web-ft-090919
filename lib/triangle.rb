d37c465d8ff8a856ab1e5725d0de3d75a3f07263
Alyssa Yates <bosslys@icloud.com>
2019-09-26 17:56:38 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,10 +2,10 @@ class Triangle
 
   attr_accessor :equilateral
 
-  equilateral_triangle = (2, 2, 2)
+
 
   def initialize(equilateral:)
     @equilateral = equilateral
-    equilateral_triangle = equilateral
   end
+  
 end
