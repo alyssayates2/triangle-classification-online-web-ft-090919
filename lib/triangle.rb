413195dae01d730b34e30f75e73c910b408ba8da
Alyssa Yates <bosslys@icloud.com>
2019-09-26 17:52:52 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,9 @@ class Triangle
   end
 
   def equilateral_triangle(equilateral)
-    if equilateral_triangle[0] != equilateral_triangle[1]
+    if equilateral_triangle[0] == equilateral_triangle[1] == equilateral_triangle[2]
+      puts "I'm an equilateral triangle!"
+    else 
       puts "I'm not an equilateral triangle!"
     end
   end
