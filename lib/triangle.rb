76c5e7bf838cda786468ff0c14b41fd50e4d113f
Alyssa Yates <bosslys@icloud.com>
2019-09-26 17:05:33 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,2 +1,16 @@
 class Triangle
+  
+  attr_accessor :equilateral  
+
+  equilateral_triangle = [num1, num2, num3]
+
+  def initialize(equilateral:)
+    @equilateral = equilateral
+  end 
+
+  def equilateral_triangle(equilateral)
+    if equilateral[i] != equilateral
+      puts  
+  end 
+  
 end 
