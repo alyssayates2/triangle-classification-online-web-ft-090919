3886cfe790c0c2ae9af3dcbd29a506579c636bfd
Alyssa Yates <bosslys@icloud.com>
2019-09-26 16:45:47 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,18 +1,2 @@
 class Triangle
-  attr_accessor :equilateral, :isosceles, :scalene
-
-  def initialization(equilateral:, isosceles:, scalene:)
-    @equilateral = equilateral
-    @isosceles = isosceles
-    @scalene = scalene
-  end
-
-  def equilateral_sides(equilateral)
-    self.equilateral = equilateral_sides
-    equilateral_sides.equilateral = self
-    equilateral_sides = [num1, num2, num3]
-  end
-
-  class TriangleError < StandardError
-  end
-end
+end 
