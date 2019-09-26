4a432d0834e650a4eaa430eff7d92ae43d284bff
Alyssa Yates <bosslys@icloud.com>
2019-09-26 18:08:07 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,11 +1,39 @@
 class Triangle
+  attr_accessor :x, :y, :z
+  def initialize(x, y, z)
+    @x = x
+    @y = y
+    @z = z
+  end
 
-  attr_accessor :equilateral
-
-
-
-  def initialize(equilateral:)
-    @equilateral = equilateral
+  def kind
+    if kosher_triangle == true && kosher_triangle2 == true 
+      puts true 
+    else 
+      raise TriangleError 
+    end 
+    
+    if x == y && y == z && x == z
+      :equilateral
+    elsif x != y && y != z && z != x
+      :scalene 
+    else 
+      :isosceles
+    end 
   end
   
+  def kosher_triangle 
+    x.positive? && y.positive? && z.positive?
+  end 
+  
+  def kosher_triangle2 
+    x + y > z && x + z > y && y + z > x
+  end 
 end
+
+class TriangleError < StandardError
+  
+end
+
+a = Triangle.new(5, 5, 5)
+a.kind
