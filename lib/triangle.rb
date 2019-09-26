05d1ed728d62e15159b086671e4a896ecb2a651c
Alyssa Yates <bosslys@icloud.com>
2019-09-26 16:31:00 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -5,15 +5,15 @@ class Triangle
     @equilateral = equilateral
     @isosceles = isosceles
     @scalene = scalene
-  end 
+  end
 
   def equilateral_sides(equilateral)
     self.equilateral = equilateral_sides
-    equilateral_sides.equilateral = self 
+    equilateral_sides.equilateral = self
     equilateral_sides = [num1, num2, num3]
-    if num1 == num2 == num3 
+    if num1 == num2 == num3
       puts equilateral
-    else 
+    else
       other 
-  end 
+  end
 end
